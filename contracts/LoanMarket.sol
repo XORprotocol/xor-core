d5d22cbb2912a560f34d5778003c4a2d70b55eff
Matthew Black <mblackmblackmblack@gmail.com>
2018-02-25 11:24:31 -0500

    Working LoanMarket

---
+++ contracts/LoanMarket.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.18;
+
+import './zeppelin/lifecycle/Killable.sol';
+
+contract LoanMarket is Killable {
+
+  struct Market {
+    uint requestPeriod; // in seconds
+    uint loanPeriod; // in seconds
+    mapping (address => uint) lenderAmounts;
+    address[] lenders;
+    mapping (address => uint) borrowerAmounts;
+    address[] borrowers;
+  }
+  
+  struct LoanData {
+    uint amount;
+    address user;
+  }
+  
+  Market[] public markets;
+
+  function createMarket(uint _requestPeriod, uint _loanPeriod) {  
+    address[] memory lenders;
+    address[] memory borrowers;
+    markets.push(Market(_requestPeriod, _loanPeriod, lenders, borrowers));
+  }
+}
+  
