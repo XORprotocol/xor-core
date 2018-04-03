da7d58de23c1b16359f56818cbe07ad1b4be8695
RunRafter <tcai534@gmail.com>
2018-04-03 00:43:26 -0400

    Added interface for trust score -> programmability

--- contracts/MarketInterest.sol
+++ contracts/MarketInterest.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.18;
 
-import './MarketTime.sol';
+import './MarketTrust.sol';
 
 /**
   * @title MarketInterest
@@ -10,7 +10,7 @@ import './MarketTime.sol';
 
  */
 
-contract MarketInterest is MarketTime {
+contract MarketInterest is MarketTrust {
 
   /*** SETTERS ***/
   /** 
@@ -30,46 +30,6 @@ contract MarketInterest is MarketTime {
   }
 
   /*** GETTERS & CALCULATIONS ***/
-  /**
-  * @dev Custom calculation of trust score for an individual borrower using square roots 
-  */
-  function getTrustScore(address _address) public view returns (uint) {
-    uint numRepayments = repayments[_address].length;
-    uint numDefaults = defaults[_address].length;
-
-    // total value of all repayments in Wei
-    uint totalRepayments;
-
-    // total value of all defaults in Wei
-    uint totalDefaults;
-
-    // numerator of calculation
-    uint repaymentComponent;
-
-    // denominator of calculation
-    uint defaultComponent;
-
-    uint score;
-
-    for (uint x = 0; x < numRepayments; x++) {
-      totalRepayments = totalRepayments.add(repayments[_address][x]);
-    }
-    for (uint y = 0; y < numDefaults; y++) {
-      totalDefaults = totalDefaults.add(defaults[_address][y]);
-    }
-
-    repaymentComponent = totalRepayments.sqrt().div(100000000);
-
-    // @note Calculation below gives defaults 1.25 weight relative to repayments of the same size
-    defaultComponent = totalDefaults.sqrt().div(80000000);
-
-    // Base trust score of 20, which is added to repaymentComponent and subtracted by defaultComponent
-    score = repaymentComponent.add(20).sub(defaultComponent);
-    // TODO: catch throw when score negative
-
-    return score;
-  }
-
   /**
   * @dev Simple custom calculation of risk factor for an individual borrower
   * @param _amt The amount being requested by borrower in current loan request
---
+++ contracts/MarketTrust.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.18;
+
+import './MarketTime.sol';
+
+/**
+ * @title MarketTrustInterface
+ * @dev Interface for custom contracts calculating trust score
+ */
+
+contract MarketTrustInterface {
+  /**
+  * @dev Calculates trust score for borrowers which will be used to determine
+  *      their risk factor and rate of interest
+  * @param _address Address of individual being checked
+  */ 
+  function getTrustScore(address _address) public view returns (uint);
+}
+
+/**
+ * @title MarketTrust
+ * @dev Contract handling logic to calculate trust score of a given borrower
+ */
+contract MarketTrust is MarketTime {
+  MarketTrustInterface trustContract;
+  
+  /**
+  * @dev Calculates trust score for borrowers by interfacing with a custom Market 
+  *      Trust Contract which will be used to determine their risk factor and 
+  *      rate of interest
+  * @param _address Address of individual being checked
+  */ 
+  function getTrustScore(uint _marketId, address _address) public view returns (uint) {
+    Market storage curMarket = markets[_marketId];
+	trustContract = MarketTrustInterface(curMarket.trustScoreAddress);
+	trustContract.getTrustScore(_address);
+  }
+
+}
