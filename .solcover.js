4f732d9533fdc8d6c575767887ff545e930f5153
Matthew Black <mblackmblackmblack@gmail.com>
2018-04-21 23:23:25 -0400

    add solcover

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,12 @@
+// NOTE: Upgrading to solidity-coverage 0.4.x breaks our tests
+
+const libFiles = require('glob').sync('contracts/lib/**/*.sol').map(n => n.replace('contracts/', ''))
+const interfaces = ['zeppelin-solidity', 'xor-libraries', 'xor-external-contract-examples']
+
 module.exports = {
-    copyPackages: ['zeppelin-solidity', 'xor-libraries', 'xor-external-contract-examples'],
-};
+    norpc: true,
+    compileCommand: '../node_modules/.bin/truffle compile',
+    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
+    skipFiles: interfaces.concat(libFiles),
+    copyNodeModules: true,
+}
